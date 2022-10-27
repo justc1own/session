a22adc60ad3fdce5dd73f5b4933d31c95f6fe7d2
justc1own <101011257+justc1own@users.noreply.github.com>
2022-10-27 13:09:46 +0300

    Add files via upload
    factorial

---
+++ factorial.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+long long getFactorial(long long x) {
+	long long res = 1;
+	if (x > 20) {
+		return -1;
+	}
+	if (x == 0 || x == 1) {
+		return res;
+	}
+	for (long long i = 2; i <= x; i++) {
+		res *= i;
+	}
+	return res;
+}
